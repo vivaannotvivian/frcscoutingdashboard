2bdcb8bf71eb6fda8a4717aada58c7779836ae23
Vivaan Singh <vsingh7205@outlook.com>
2026-01-30 20:28:04 -0500

    corrected sign in with google

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -44,7 +44,7 @@ export function AuthProvider({ children }) {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: window.location.origin
+                redirectTo: 'https://vivaannotvivian.github.io/frcscoutingdashboard'
             }
         });
         if (error) throw error;
